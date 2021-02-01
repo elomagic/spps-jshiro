9a2ef2a52e1e3e1e9501dd7c2a92142b3a3bdee6
C. Rambow <dev@elomagic.de>
2021-02-01 17:41:43 +0100

    API changed

--- src/main/java/de/elomagic/spps/jshiro/SimpleCrypt.java
+++ src/main/java/de/elomagic/spps/jshiro/SimpleCrypt.java
@@ -26,8 +26,10 @@ import org.apache.shiro.codec.CodecSupport;
 import org.apache.shiro.crypto.AesCipherService;
 import org.apache.shiro.crypto.DefaultBlockCipherService;
 import org.apache.shiro.util.ByteSource;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -44,7 +46,6 @@ import java.util.Collections;
 public final class SimpleCrypt {
 
     private static final Logger LOGGER = LogManager.getLogger(SimpleCrypt.class);
-    // Must be moved to the users home folder
     private static final Path MASTERKEY_FILE = Paths.get(System.getProperty("user.home"), ".elomagic", "masterkey");
     private static final DefaultBlockCipherService CIPHER = new AesCipherService();
 
@@ -74,37 +75,70 @@ public final class SimpleCrypt {
         return result;
     }
 
+
     /**
      * Encrypt, encoded as Base64 and encapsulate with curly bracket of a string.
      *
-     * @param decrypted a non encrypted string
-     * @return Returns a encrypted string
-     * @throws IOException Thrown when unable to read the master key
+     * @param decrypted a non encrypted byte array
+     * @return Returns a encrypted, Base64 encoded string, surrounded with curly brackets.
+     * @throws GeneralSecurityException Thrown when an error occurred during encrypting.
      */
-    public static String encrypt(String decrypted) throws IOException {
-        try {
-            byte[] secretBytes = CodecSupport.toBytes(decrypted, "utf-8");
+    @Nullable
+    public static String encrypt(@Nullable byte[] decrypted) throws GeneralSecurityException {
+        if (decrypted == null) {
+            return null;
+        }
 
-            ByteSource encrypted = CIPHER.encrypt(secretBytes, getMasterKey());
+        try {
+            ByteSource encrypted = CIPHER.encrypt(decrypted, getMasterKey());
 
             return "{" + encrypted.toBase64() + "}";
         } catch(Exception ex) {
             LOGGER.error(ex.getMessage(), ex);
-            throw ex;
+            throw new GeneralSecurityException(ex.getMessage(), ex);
         }
     }
 
     /**
+     * Encrypt, encoded as Base64 and encapsulate with curly bracket of a string.
+     *
+     * @param decrypted a non encrypted char array
+     * @return Returns a encrypted, Base64 encoded string, surrounded with curly brackets.
+     * @throws GeneralSecurityException Thrown when an error occurred during encrypting.
+     */
+    @Nullable
+    public static String encrypt(@Nullable char[] decrypted) throws GeneralSecurityException {
+        return decrypted == null ? null : encrypt(new String(decrypted).getBytes(StandardCharsets.UTF_8));
+    }
+
+    /**
+     * Encrypt, encoded as Base64 and encapsulate with curly bracket of a string.
+     *
+     * @param decrypted a non encrypted string
+     * @return Returns a encrypted, Base64 encoded string, surrounded with curly brackets.
+     * @throws GeneralSecurityException Thrown when an error occurred during encrypting.
+     */
+    @Nullable
+    public static String encrypt(@Nullable String decrypted) throws GeneralSecurityException {
+        return decrypted == null ? null : encrypt(decrypted.getBytes(StandardCharsets.UTF_8));
+    }
+
+
+    /**
      * Decrypt an encrypted, Base64 encoded data string.
      *
-     * @param encryptedBase64 Base64 encoded data string, encapsulated with curly bracket.
-     * @return The encrypted data as string
-     * @throws IOException Thrown when input data not encapsulated with curly bracket.
+     * @param encryptedBase64 Base64 encoded data string, encapsulated with curly brackets.
+     * @return The encrypted data as string.
      * @throws GeneralSecurityException Thrown when unable to decrypt data .
      */
-    public static String decrypt(String encryptedBase64) throws IOException, GeneralSecurityException {
+    @Nullable
+    public static byte[] decrypt(@Nullable String encryptedBase64) throws GeneralSecurityException {
+        if (encryptedBase64 == null) {
+            return null;
+        }
+
         if(!isEncryptedValue(encryptedBase64)) {
-            throw new IOException("This value is not encapsulated as an encrypted value. Unable to decrypt.");
+            throw new GeneralSecurityException("This value is not with curly brackets encapsulated as an encrypted value. Unable to decrypt.");
         }
 
         try {
@@ -113,11 +147,35 @@ public final class SimpleCrypt {
 
             ByteSource decrypted = CIPHER.decrypt(encryptedBytes, getMasterKey());
 
-            return CodecSupport.toString(decrypted.getBytes(), "utf-8");
+            return decrypted.getBytes();
         } catch(Exception ex) {
             LOGGER.error(ex.getMessage(), ex);
             throw new GeneralSecurityException("Unable to decrypt data.", ex);
         }
+    }
+
+    /**
+     * Decrypt an encrypted, Base64 encoded data string.
+     *
+     * @param encryptedBase64 Base64 encoded data string, encapsulated with curly brackets.
+     * @return The encrypted data as char array.
+     * @throws GeneralSecurityException Thrown when unable to decrypt data .
+     */
+    @Nullable
+    public static char[] decryptToChars(@Nullable String encryptedBase64) throws GeneralSecurityException {
+        return encryptedBase64 == null ? null : CodecSupport.toChars(decrypt(encryptedBase64));
+    }
+
+    /**
+     * Decrypt an encrypted, Base64 encoded data string.
+     *
+     * @param encryptedBase64 Base64 encoded data string, encapsulated with curly brackets.
+     * @return The encrypted data as string.
+     * @throws GeneralSecurityException Thrown when unable to decrypt data .
+     */
+    @Nullable
+    public static String decryptToString(@Nullable String encryptedBase64) throws GeneralSecurityException {
+        return encryptedBase64 == null ? null : new String(decrypt(encryptedBase64), StandardCharsets.UTF_8);
     }
 
     /**
@@ -126,7 +184,7 @@ public final class SimpleCrypt {
      * @param value Value to be checked
      * @return Returns true when value is identified as an encrypted value.
      */
-    public static boolean isEncryptedValue(String value) {
+    public static boolean isEncryptedValue(@Nullable String value) {
         return value != null && value.startsWith("{") && value.endsWith("}");
     }
 
--- src/test/java/de/elomagic/spps/jshiro/SimpleCryptTest.java
+++ src/test/java/de/elomagic/spps/jshiro/SimpleCryptTest.java
@@ -17,29 +17,42 @@
  */
 package de.elomagic.spps.jshiro;
 
+import org.apache.shiro.codec.CodecSupport;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
 
 class SimpleCryptTest {
 
-    public SimpleCryptTest() {
-    }
-
     @Test
-    void testEncryptDecrypt() throws Exception {
-        System.out.println("encrypt");
-
+    void testEncryptDecryptWithString() throws Exception {
         String value = "secret";
 
         String encrypted = SimpleCrypt.encrypt(value);
 
         Assertions.assertNotEquals(value, encrypted);
 
-        String decrypted = SimpleCrypt.decrypt(encrypted);
+        String decrypted = SimpleCrypt.decryptToString(encrypted);
 
         Assertions.assertEquals(value, decrypted);
+    }
+
+    @Test
+    void testEncryptDecryptWithChars() throws Exception {
+        String value = "secretäöüß";
+
+        char[] chars = CodecSupport.toChars(value.getBytes(StandardCharsets.UTF_8));
+
+        String encrypted = SimpleCrypt.encrypt(chars);
+
+        Assertions.assertNotEquals(value, encrypted);
+
+        char[] decryptedChars = SimpleCrypt.decryptToChars(encrypted);
+
+        Assertions.assertArrayEquals(chars, decryptedChars);
     }
 
     /**
@@ -47,17 +60,14 @@ class SimpleCryptTest {
      */
     @Test
     void testIsEncryptedValue() {
-        System.out.println("isEncryptedValue");
-        String value = "";
-
         Assertions.assertTrue(SimpleCrypt.isEncryptedValue("{abc}"));
         Assertions.assertFalse(SimpleCrypt.isEncryptedValue("abc}"));
     }
 
     @Test
-    void testDecrypt1() throws Exception {
-        IOException ex = Assertions.assertThrows(IOException.class, ()->SimpleCrypt.decrypt("this isn't a encapsulated value"));
-        Assertions.assertTrue(ex.getMessage().contains("This value is not encapsu"));
+    void testDecrypt1() {
+        Exception ex = Assertions.assertThrows(GeneralSecurityException.class, ()->SimpleCrypt.decrypt("this isn't a encapsulated value"));
+        Assertions.assertTrue(ex.getMessage().contains("This value is not with curly brackets"));
     }
 
 }
